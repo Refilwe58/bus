77efe9d9329efccb8a4559f158eb7fafeeab3ca4
refilwe58 <paledirefilwe@gmail.com>
2025-08-04 14:48:32 +0200

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ import bodyParser from 'body-parser';
 
 dotenv.config({ path: './backend/.env' });
 const port = 5000;
-//console.log('Stripe Key:', process.env.STRIPE_SECRET_KEY); 
-const stripe =new Stripe( process.env.STRIPE_SECRET_KEY);
+console.log('Stripe Key:', process.env.STRIPE_SECRET_KEY); 
+const stripe =new Stripe(process.env.STRIPE_SECRET_KEY);
 
 import { handleLogin } from './login.js';
 import {users} from './data.js'
